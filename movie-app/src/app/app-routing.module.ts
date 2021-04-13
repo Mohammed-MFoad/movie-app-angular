b8b5188ac069247909b135de92e0c5bca6bc1edb
Mohammed-MFoad <mohammed.mfoad@gmail.com>
2021-04-13 03:48:58 +0200

    add-movie-details

--- movie-app/src/app/app-routing.module.ts
+++ movie-app/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
+import { MovieDetailsComponent } from './components/movies/movie-details/movie-details.component';
 import { MoviesComponent } from './components/movies/movies/movies.component';
 
 const routes: Routes = [
@@ -9,6 +10,7 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'movies', component: MoviesComponent },
+  { path: 'movie/:id',component: MovieDetailsComponent},
 ];
 
 @NgModule({
--- movie-app/src/app/components/movies/movie-details/movie-details.component.ts
+++ movie-app/src/app/components/movies/movie-details/movie-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { MoviesService } from '../../../shared/services/movies/movies.service';
 
 @Component({
   selector: 'app-movie-details',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-details.component.scss']
 })
 export class MovieDetailsComponent implements OnInit {
+  movie!: any;
+  related: any[] = []
+  img: string = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/"
+  img2: string = "https://www.themoviedb.org/t/p/w250_and_h141_face/"
 
-  constructor() { }
+  constructor( private activatedRoute: ActivatedRoute,
+    private movieService: MoviesService) { }
 
   ngOnInit() {
+    this.activatedRoute.params
+    .pipe(
+      switchMap( ({id}) => this.movieService.getMovie(id) )
+    )
+    .subscribe( (movie: any) => {
+      this.movie = movie;
+    })
+
   }
 
 }
