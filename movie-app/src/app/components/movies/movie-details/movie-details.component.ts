5825649307fff03f67c5eeb6ceaf870a882d86d5
Mohammed-MFoad <mohammed.mfoad@gmail.com>
2021-04-13 10:25:56 +0200

    add-comments

--- movie-app/src/app/components/movies/movie-details/movie-details.component.ts
+++ movie-app/src/app/components/movies/movie-details/movie-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
+import { AuthService } from 'src/app/shared/services/auth/auth.service';
 import { MoviesService } from '../../../shared/services/movies/movies.service';
 
 @Component({
@@ -14,18 +15,60 @@ export class MovieDetailsComponent implements OnInit {
   img: string = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/"
   img2: string = "https://www.themoviedb.org/t/p/w250_and_h141_face/"
 
+  comments:any =  []
+  emailSignedIn:string;
+  comment:string;
+  movieId:string;
+
   constructor( private activatedRoute: ActivatedRoute,
-    private movieService: MoviesService) { }
+    private movieService: MoviesService,
+    public authService: AuthService,) { 
+
+     
+      
+    }
 
   ngOnInit() {
+    this.authService.user.subscribe(user => this.emailSignedIn = user.email)
     this.activatedRoute.params
     .pipe(
-      switchMap( ({id}) => this.movieService.getMovie(id) )
-    )
-    .subscribe( (movie: any) => {
-      this.movie = movie;
-    })
-
+      switchMap( ({id}) => this.movieService.getMovie(id)  )
+      )
+      .subscribe( (movie: any) => {
+        this.movie = movie;
+        this.movieId = movie.id
+        this.comments =JSON.parse( localStorage.getItem("commentsFor"+this.movieId));
+        console.log(this.movieId);
+        console.log(this.comments);
+      });
+    
+  }
+  getComments(){
+    
+  }
+  appendComment()
+  {
+    console.log(this.movieId);
+    console.log(this.comments);
+    console.log(this.emailSignedIn);
+    console.log(this.comment);
+    
+    let newComment = {
+      email:this.emailSignedIn,
+      comment:this.comment
+    }
+    if(this.comments){
+      this.comments.push(newComment)
+       console.log(this.comments);
+      localStorage.setItem(("commentsFor"+this.movieId), (JSON.stringify(this.comments)))
+    }
+    else{
+      this.comments=[]
+      this.comments.push(newComment)
+      console.log(this.comments);
+      localStorage.setItem(("commentsFor"+this.movieId), (JSON.stringify(this.comments)))
+    }
+    this.comment = ""
   }
 
 }
